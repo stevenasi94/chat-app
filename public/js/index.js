fbf36a9215400f7c069ff0be66d2a3fcbbafae72
DESKTOP-91F0BB8\stevn <stevnasi@gmail.com>
2018-04-22 17:16:09 +1000

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,17 +2,12 @@ var socket = io();
 
 socket.on("connect", function () {
     console.log("Connected to server");
-
-    socket.emit("createMessage", {
-        from: "jen@example.com",
-        text: "Hey. This is Steven."
-    });
 });
 
 socket.on("disconnect", function () {
     console.log("Disconnected from server");
 });
 
-socket.on("newMessage", function (Message) {
-    console.log("New message", Message);
+socket.on("newMessage", function (message) {
+    console.log("New message", message);
 });
